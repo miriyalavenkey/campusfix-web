0757511934bb942e43993f205c8f890a5f48d68e
miriyalavenkey <miriyalavenkey43@gmail.com>
2026-09-20 11:09:39 +0530

    Delete CampusFix_Web/api/index.py

--- CampusFix_Web/api/index.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-
-# Add root directory to sys.path so app and database can be discovered
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from app import app
